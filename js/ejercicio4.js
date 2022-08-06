36c22169ba708dd0674f7b037fc53e2db2760ad0
Daiana Leiva <dayleiva92@gmail.com>
2022-08-05 23:34:46 -0300

    Guardo del ejercicio uno al cinco

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,3 +1,8 @@
 /*4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
 
 */
+let nombreUsuario = "Daiana";
+
+nombreUsuario = prompt('Ingrese su nombre. Gracias');
+console.log(nombreUsuario);
+document.write('<h2>Hola ' + (nombreUsuario) + '</h2>');
---
+++ js/ejercicio5.js
@@ -0,0 +1,12 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// Nota: Tener en cuenta la siguiente función: parseInt
+
+let numberUno = parseInt(prompt('Ingrese el primer numero'));
+let numberDos = parseInt(prompt('Ingrese el segundo numero'));
+
+console.log(numberUno);
+console.log(numberDos);
+
+document.write('<h3>El resultado de su suma es: ' + (numberUno + numberDos) + '</h3>');
+
+
